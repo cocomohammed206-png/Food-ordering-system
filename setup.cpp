bad4f6efe87e84cf5c16c1d73f3df42ce965e957
hamza2112-coder <hamzamohamed132h@gmail.com>
2026-04-10 16:11:16 +0200

    Clean up setup.cpp by removing unused code
    Removed commented-out code for total bill calculation.

--- setup.cpp
+++ setup.cpp
@@ -31,23 +31,5 @@ int main() {
 
     setupMenu(menuItems, prices, quantities);
 
-   
-
-    // Hamzaa//
-    double totalBill = 0; 
-    
-    for (int i = 1; i < SIZE; i++) {
-        if (quantities[i] > 0) {
-            // y7sb el sandwich bl 3dd
-            double subtotal = quantities[i] * prices[i];
-            
-            // yl3 hesab el sandwich lwhdu
-            cout << menuItems[i] << " x" << quantities[i] << " = " << subtotal << " PHP" << endl;
-            
-            // yhsb total bill
-            totalBill += subtotal;
-        }
-    }
-
     return 0;
 }
