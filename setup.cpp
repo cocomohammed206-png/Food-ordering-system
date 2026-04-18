ed7a2b84a180216bcef45ac1ce4193eb1b600f70
Malak Mohammed <coco.mohammed.206@gmail.com>
2026-04-18 22:56:13 +0200

    Refactor menu items and prices initialization

--- setup.cpp
+++ setup.cpp
@@ -1,31 +1,12 @@
 #include <iostream>
 #include <string>
+#include <iomanip> // For nice spacing
+
 using namespace std;
 
 const int SIZE = 6;
 
 int main() {
-    string menuItems[SIZE];
-    double prices[SIZE];
-    int quantities[SIZE];
-
-    menuItems[0] = "Burger";
-    menuItems[1] = "Pizza";
-    menuItems[2] = "Pasta";
-    menuItems[3] = "Sandwich";
-    menuItems[4] = "Fries";
-    menuItems[5] = "Drink";
-
-    prices[0] = 195;
-    prices[1] = 120;
-    prices[2] = 75;
-    prices[3] = 85;
-    prices[4] = 45;
-    prices[5] = 25;
-
-    for (int i = 0; i < SIZE; i++) {
-        quantities[i] = 0;
-    }
-
-    return 0;
-}
+    string menuItems[SIZE] = {"Burger", "Pizza", "Pasta", "Sandwich", "Fries", "Drink"};
+    double prices[SIZE] = {195, 120, 75, 85, 45, 25};
+    int quantities[SIZE] = {0};
