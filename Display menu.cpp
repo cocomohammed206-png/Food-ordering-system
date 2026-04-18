7b9c8a4677c23b55d7bfb528dd5d85e76063e2d5
nouressbahgat-svg <nouressbahgat@gmail.com>
2026-04-18 22:33:44 +0200

    Fix item numbering in menu display

--- Display menu.cpp
+++ Display menu.cpp
@@ -4,7 +4,7 @@ void DisplayMenu(string items[],double prices[],int size) {
 
         for(int i=0; i<size; i++) {
 
-            cout<<i<<"\t"<<left<<setw(15)<<items[i]<<prices[i]<<"EGP"<<endl;
+            cout<<i+1<<"\t"<<left<<setw(15)<<items[i]<<prices[i]<<"EGP"<<endl;
 
         }
 
