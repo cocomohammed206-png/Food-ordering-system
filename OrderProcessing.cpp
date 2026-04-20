8b72c9268fa8a62af0cdac490bfa4ba44d4888a7
nouressbahgat-svg <nouressbahgat@gmail.com>
2026-04-20 03:27:13 +0200

    Implement order selection and quantity input
    Added a loop for order processing with quantity validation.

--- OrderProcessing.cpp
+++ OrderProcessing.cpp
@@ -1,1 +1,33 @@
-.
+int pick ;
+cout << "Crafted with love, served with flavor."<<endl ;
+while (true)
+{
+     DisplayMenu(menuItems,prices,SIZE);
+     cout<<"Take your pick happiness is waiting"<<endl;
+     cin>>pick ;
+       if (pick == SIZE + 1)
+         {
+           break;
+         }
+        if (pick >= 1 && pick <= SIZE)
+        {
+              int qty;
+              cout << "How many " << menuItems[pick - 1] << "? ";
+              cin >> qty;
+              while (qty <= 0)
+                {
+                  cout << "Invalid quantity! Please enter a number greater than 0: ";
+                   cin >> qty;
+                }
+                  if (qty > 0)
+                    {
+                      quantities[pick - 1] += qty;
+                    }
+        }
+         else 
+            {
+            cout << "Wrong choice ,pleas choose from menu!" << endl;
+            }
+        cout <<"do you want another thing ?"<<endl;
+
+}
