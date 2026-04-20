7e14fe770bfce11f735e8d047a8bbaaa959686cf
hamza2112-coder <hamzamohamed132h@gmail.com>
2026-04-20 14:25:08 +0200

    Update billcalc.cpp

--- billcalc.cpp
+++ billcalc.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
 #include <string>
 using namespace std;
-    // Hamzaa//
-    double totalBill = 0; 
-    
-    for (int i = 1; i < SIZE; i++) {
+
+double displayReceipt(string menuItems[], int quantities[], double prices[], int SIZE) {
+    double totalBill = 0; //to display the recipt
+
+    for (int i = 0; i < SIZE; i++) { 
         if (quantities[i] > 0) {
-            // y7sb el sandwich bl 3dd
-            double subtotal = quantities[i] * prices[i];
-            
-            // yl3 hesab el sandwich lwhdu
+            double subtotal = quantities[i] * prices[i]; //da 3shan ydrb el quantity f s3r
             cout << menuItems[i] << " x" << quantities[i] << " = " << subtotal << " PHP" << endl;
-            
-            // yhsb total bill
             totalBill += subtotal;
         }
     }
- 
+    return totalBill; 
+}
