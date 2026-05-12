19ad7cce8853ba28b2211b47e0ef224541715bbb
Malak Mohammed <coco.mohammed.206@gmail.com>
2026-05-12 10:09:11 +0300

    Remove discount constants from code
    Removed discount constants for menu calculations.

--- NEW FULL CODE.cpp
+++ NEW FULL CODE.cpp
@@ -11,9 +11,6 @@ const int DESSERTS_SIZE = 3;
 const int CAT_SIZE = 3;
 const int SIZE = MEALS_SIZE + DRINKS_SIZE + DESSERTS_SIZE;
 
-const double DISCOUNT_10_MIN = 300.0;
-const double DISCOUNT_20_MIN = 600.0;
-
 void DisplayMenu
      (string arr1[], string arr2[], string arr3[],
      double prices1[], double prices2[], double prices3[],
